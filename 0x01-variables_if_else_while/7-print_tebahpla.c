abf1b740c5ba45c5d8f4f6d57e221856c229cbfc
Ruttz08 <ruttizy14@gmail.com>
2023-04-23 06:20:35 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main- Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+	putchar(low);
+	putchar('\n');
+	return (0);
+}
