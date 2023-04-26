eb686b07f9ad27821a5f4451a204ac8741df74c8
Ruttz08 <ruttizy14@gmail.com>
2023-04-25 18:51:34 -0700

    103-fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,16 +9,22 @@
 int main(void)
 {
 	int i = 0;
-	long j = 1, k = 2, sum = k;
+	long j = 1, k = 2;
 
-	while (k + j < 0000000)
+	while (i < 50)
+	{
+	if (i == 0)
+	printf("%ld", j);
+	else if (i == 1)
+	printf(", %ld", k);
+	else
 	{
 	k += j;
-	if (k % 2 == 0)
-	sum += j;
 	j = k - j;
+	printf(", %ld", k);
+	}
 	++i;
 	}
-	printf("%\d\n", sum);
+	printf("\n");
 	return (0);
 }
