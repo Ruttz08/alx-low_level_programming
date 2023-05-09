7c9c85c6c504b36b5130722839dd4a284fc18ff4
Ruttz08 <ruttizy14@gmail.com>
2023-05-09 11:25:10 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn -  Functions of the length of prefix substring
+ * @s: byte
+ * @accept: given point
+ * Return: 0 (Success)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int k, n, value, check;
+
+	value = 0;
+
+	for (k = 0; s[k] != '\0';  k++)
+	{
+		check = 0;
+
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (accept[n] == s[k])
+			{
+				value++;
+				check = 1;
+			}
+		}
+		
+		if (check == 0)
+		{
+			return (value);
+		}
+	}
+	return (n);
+}	
