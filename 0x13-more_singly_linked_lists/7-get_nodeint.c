35fc699860483ef711c44a5af72601c75fee784e
Ruttz08 <ruttizy14@gmail.com>
2023-06-06 16:46:24 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,12 +13,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 	listint_t *tmp = head;
-	
+
 	while (tmp && i < index)
 	{
 		tmp = tmp->next;
 		i++;
 	}
-	
 	return (tmp ? tmp : NULL);
 }
